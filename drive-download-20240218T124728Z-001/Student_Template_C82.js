db716b5b06a4930e460173b77c3f2f9c8617c484
Aadith-H <131546566+Aadith-H@users.noreply.github.com>
2024-02-18 08:13:08 -0500

    Add files via upload

---
+++ drive-download-20240218T124728Z-001/Student_Template_C82.js
@@ -0,0 +1,33 @@
+console.log("My first console project");
+
+            var first_name = "Aadith";
+            var last_name = "Hiremath";
+
+
+first_name.concat(last_name)
+
+            var full_name = AadithHiremath ;
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+
+ var add = a + b
+
+Type: console.log(add)
+
+            var add = a + b ;
+            console.log(add);
+Type: var sub = a - b
+
+            var sub = a- b ;
+            console.log(sub);
+
+
+
+            var multiple = a * b ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
